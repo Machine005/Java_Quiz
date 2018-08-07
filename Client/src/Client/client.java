1439860635349f883be400747011823540408efb
John_Reese <18565592961@163.com>
2018-08-07 23:07:36 +0800

    3

---
+++ Client/src/Client/client.java
@@ -0,0 +1,70 @@
+package Client;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class client {
+	private byte buffer[]=new byte[10];
+	private Socket sck;
+	private OutputStream os;
+	private InputStream is;
+	
+	void socketconnet(String ip,int port) {
+		while(sck==null) {
+		try {
+			
+			sck=new Socket(ip,port);
+			os=sck.getOutputStream();
+			is=sck.getInputStream();
+			
+			//while(is.read(buffer)!=-1) {
+			//	System.out.print(new String(buffer));
+			//}
+			//System.out.println();
+			
+		} catch (UnknownHostException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+	 }
+    }	
+	void File_read_send(String file) {
+		try {
+			FileInputStream fr=new FileInputStream(file);
+			while(fr.read(buffer)!=-1) {
+			os.write(buffer);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+	}
+	void Socket_close() {
+		try {
+			sck.close();
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		client c=new client();
+		//System.out.println("输入ip：");
+		c.socketconnet("192.168.101.3",182);
+		//System.out.println("输入port：");
+		c.File_read_send("C:\\Users\\asus\\Desktop\\123.txt");
+		c.Socket_close();
+	}
+
+}
---
+++ Server/src/server.java
@@ -0,0 +1,51 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+	private ServerSocket ssck;
+	private Socket sck;
+	private byte buffer[]=new byte[20];
+	private InputStream is;
+	private OutputStream os;
+	
+	 void serversocket_connet(){
+		 while(sck==null) {
+		 try {
+			 
+			ssck=new ServerSocket(182);
+			sck=ssck.accept();
+			is=sck.getInputStream();
+
+		 } catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		 }
+		}
+	 }
+	 void Fileread_write(String file) {
+		 try {
+			FileOutputStream fr=new FileOutputStream(file);
+			while(is.read(buffer)!=-1) {
+				fr.write(buffer);
+			}
+		} catch (FileNotFoundException  e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		}
+	 }
+	public static void main(String[] args) {
+		
+		server s=new server();
+		s.serversocket_connet();
+		s.Fileread_write("C:\\Users\\asus\\Desktop\\123\\123.txt");
+	}
+
+}
